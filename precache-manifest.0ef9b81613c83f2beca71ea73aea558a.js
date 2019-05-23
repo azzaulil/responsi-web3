b28bbee8f5322110f1e2d1fcc88f6d044ce13f71
azzaulil <azzaulil50@gmail.com>
2019-05-23 10:34:34 +0700

    Updates

---
+++ precache-manifest.0ef9b81613c83f2beca71ea73aea558a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11a2d503f9a54e32f4cdedbabc33997",
+    "url": "/responsi-web3/index.html"
+  },
+  {
+    "revision": "fe6710d8f51c45dfe311",
+    "url": "/responsi-web3/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/responsi-web3/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fe6710d8f51c45dfe311",
+    "url": "/responsi-web3/static/js/main.62d0aa82.chunk.js"
+  },
+  {
+    "revision": "fa6a19c83f98ec53fccf",
+    "url": "/responsi-web3/static/js/runtime~main.35407e14.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/responsi-web3/static/media/logo.ee7cd8ed.svg"
+  }
+]);
